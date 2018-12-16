9a5f6a77e546e957292245e27c6e750fca5d93b9
Kasey Lawrence <kasey.lawrence@gmail.com>
2018-12-15 19:06:40 -0600

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,10 +1,18 @@
-<? php 
-if (isset($_POST['send'])) {  
-    $to = 'kaseylawrence@gmail.com';
-    $subject = 'From $name';
-    $message = 'Name: ' . $_POST['name'] . "\r\n\r\n";
-    $message .= 'Email: ' . $_POST['email'] . "\r\n\r\n"
-    $message .= 'Comments: ' . $_POST['comments'];
-    echo $message;
-}
+<?php
+
+	  $name = $_POST['name'];
+
+	  $visitor_email = $_POST['email'];
+
+	  $message = $_POST['message'];
+	  $email_from = 'yourname@yourwebsite.com';
+	  $email_subject = "New Form submission";
+	  $email_body = "You have received a new message from the user $name.\n".
+				"Here is the message:\n $message".;
+				
+	$to = "yourname@yourwebsite.com";
+	$headers = "From: $email_from \r\n";
+	$headers .= "Reply-To: $visitor_email \r\n";
+	mail($to,$email_subject,$email_body,$headers);
+	
 ?>
