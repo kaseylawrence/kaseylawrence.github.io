235a44fe7dd753721806b22be371f9321e6eadcb
Kasey Lawrence <kasey.lawrence@gmail.com>
2018-12-15 19:07:28 -0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -10,7 +10,7 @@
 	  $email_body = "You have received a new message from the user $name.\n".
 				"Here is the message:\n $message".;
 				
-	$to = "yourname@yourwebsite.com";
+	$to = "kaseylawrence@gmail.com";
 	$headers = "From: $email_from \r\n";
 	$headers .= "Reply-To: $visitor_email \r\n";
 	mail($to,$email_subject,$email_body,$headers);
